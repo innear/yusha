43707f571466ed54bb77dbc1b24d55da5360215b
embroid <wangxinyu.go@qq.com>
2022-12-28 17:27:57 +0800

    注释补充

--- client/httpclient.go
+++ client/httpclient.go
@@ -11,16 +11,20 @@ import (
 	"yusha/config"
 )
 
+// MethodNotAllowedInProxy 初始化异常信息
 var (
 	MethodNotAllowedInProxy = errors.New("the reverse proxy does not support request methods other than get and post")
 )
 
+// 全局 http client
 var yuShaHttpClient = &yuShaClient{http.Client{Timeout: time.Second * time.Duration(config.Yusha.Timeout)}}
 
+// 组合 goland 底层 http.Client 结构, 进行底层方法的重写
 type yuShaClient struct {
 	http.Client
 }
 
+// Proxy 代理转发方法
 func Proxy(r *http.Request) (resp *http.Response, err error) {
 	switch r.Method {
 	case http.MethodGet:
@@ -32,10 +36,12 @@ func Proxy(r *http.Request) (resp *http.Response, err error) {
 	}
 }
 
+// Get 重新实现 golang 底层 http.Client Get 方法
 func (ysc *yuShaClient) Get(r *http.Request) (resp *http.Response, err error) {
 	return ysc.Do(r)
 }
 
+// Post 重新实现 golang 底层 http.Client Post 方法
 func (ysc *yuShaClient) Post(r *http.Request) (resp *http.Response, err error) {
 	return ysc.Do(r)
 }
